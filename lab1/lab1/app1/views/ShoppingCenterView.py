1028d8cb4fad3090583f6f5b2a281cf4cb7f049f
927_Szekely_Bianca <bianca.szekely@stud.ubbcluj.ro>
2023-04-03 20:00:53 +0300

    modified to Single-responsibility principle

--- lab1/lab1/app1/views/ShoppingCenterView.py
+++ lab1/lab1/app1/views/ShoppingCenterView.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-from lab1.lab1.app1.models.ShoppingCenterModel import ShoppingCenter
+from app1.models.ShoppingCenterModel import ShoppingCenter
 from app1.serializer.EmployeeSerializer import EmployeeSerializer, EmployeeIdSerializer
 from app1.serializer.ShoppingCenterSerializer import ShoppingCenterSerializer, ShoppingCenterIdSerializer
 
