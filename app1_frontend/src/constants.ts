ab224ef24ac8515e6c3c407b335beac4f1a37ab7
927_Szekely_Bianca <bianca.szekely@stud.ubbcluj.ro>
2023-04-08 16:59:37 +0300

    Deploy netlify

--- app1_frontend/src/constants.ts
+++ app1_frontend/src/constants.ts
@@ -1,4 +1,4 @@
-const PROD_BACKEND_API_URL = "https://shopping_center_app.strangled.net/app1/";
+const PROD_BACKEND_API_URL = "https://shopping-center-app.strangled.net/app1/";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/app1/";
 
 export const BACKEND_API_URL =
