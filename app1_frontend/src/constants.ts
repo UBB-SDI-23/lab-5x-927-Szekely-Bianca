4b0e8055bbd0b1b4c623e86ba7ed60e4365b6d41
927_Szekely_Bianca <bianca.szekely@stud.ubbcluj.ro>
2023-04-09 19:17:39 +0300

    commit backend api url

--- app1_frontend/src/constants.ts
+++ app1_frontend/src/constants.ts
@@ -1,6 +1,5 @@
-// const PROD_BACKEND_API_URL = "http://ec2-16-170-164-151.eu-north-1.compute.amazonaws.com:8000/app1/";
-// const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/app1/";
-
-// export const BACKEND_API_URL =
-// 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
-export const BACKEND_API_URL = "http://ec2-16-16-91-55.eu-north-1.compute.amazonaws.com/app1";
+const PROD_BACKEND_API_URL = "/app1";
+const DEV_BACKEND_API_URL = "http://ec2-16-16-91-55.eu-north-1.compute.amazonaws.com/app1";
+export const BACKEND_API_URL =
+	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
+// export const BACKEND_API_URL = "http://ec2-16-16-91-55.eu-north-1.compute.amazonaws.com/app1";
