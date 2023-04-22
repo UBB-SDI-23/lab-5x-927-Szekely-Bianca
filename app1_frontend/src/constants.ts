45f06aa9db714b6b6102c08ce3ad65b66d33da1e
927_Szekely_Bianca <bianca.szekely@stud.ubbcluj.ro>
2023-04-22 20:17:07 +0300

    deploy on netlify

--- app1_frontend/src/constants.ts
+++ app1_frontend/src/constants.ts
@@ -1,6 +1,13 @@
-const PROD_BACKEND_API_URL = "/app1";
-const DEV_BACKEND_API_URL = "http://ShoppingCenter.mooo.com/app1";
-export const BACKEND_API_URL =
-	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
+//const PROD_BACKEND_API_URL = "/app1";
+//const DEV_BACKEND_API_URL = "http://ShoppingCenter.mooo.com/app1";
+//export const BACKEND_API_URL =
+//	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
  //export const BACKEND_API_URL = "http://ec2-13-53-45-100.eu-north-1.compute.amazonaws.com/app1";
 ///app1/* http://ec2-16-16-91-55.eu-north-1.compute.amazonaws.com/:splat 200!
+
+
+const PROD_BACKEND_API_URL = "https://ShoppingCenter.mooo.com";
+const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
+
+export const BACKEND_API_URL =
+	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
