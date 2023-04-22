f118f6fa4c1ed6eb0e48f99cd6c7842c54113f23
927_Szekely_Bianca <bianca.szekely@stud.ubbcluj.ro>
2023-04-22 18:26:31 +0300

    deploy on netlify

--- app1_frontend/src/constants.ts
+++ app1_frontend/src/constants.ts
@@ -1,5 +1,5 @@
 const PROD_BACKEND_API_URL = "/app1";
-const DEV_BACKEND_API_URL = "http://ec2-16-16-115-56.eu-north-1.compute.amazonaws.com/app1";
+const DEV_BACKEND_API_URL = "http://ec2-13-50-246-102.eu-north-1.compute.amazonaws.com/app1";
 export const BACKEND_API_URL =
 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
  //export const BACKEND_API_URL = "http://ec2-13-53-45-100.eu-north-1.compute.amazonaws.com/app1";
