5ef47fe0d645048845980a16fc82d9227000c705
927_Szekely_Bianca <bianca.szekely@stud.ubbcluj.ro>
2023-04-22 23:33:36 +0300

    deploy on netlify

--- app1_frontend/src/constants.ts
+++ app1_frontend/src/constants.ts
@@ -4,9 +4,10 @@
 //	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
  //export const BACKEND_API_URL = "http://ec2-13-53-45-100.eu-north-1.compute.amazonaws.com/app1";
 ///app1/* http://ec2-16-16-91-55.eu-north-1.compute.amazonaws.com/:splat 200!
+///app1/* http://ec2-13-50-246-102.eu-north-1.compute.amazonaws.com/app1/:splat 200!
+// /*   /index.html   200
 
-
-const PROD_BACKEND_API_URL = "https://ShoppingCenter.mooo.com";
+const PROD_BACKEND_API_URL = "https://ShoppingCenter.mooo.com/app1";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
 
 export const BACKEND_API_URL =
